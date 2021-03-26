965921f6562b88f6f60a3fb0014fb0270ff5ec08
Ondrej Postulka <ondrej.postulka@tietoevry.com>
2021-03-26 18:41:38 +0100

    1st commit

---
+++ app.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template, redirect, url_for, flash
+from flask_socketio import SocketIO, send
+
+app = Flask(__name__)
+app.secret_key = 'nesavjoh494+-*/'
+socketio = SocketIO(app)
+
+@socketio.on('message')
+def handleMessages(msg):
+    print('Message: ' + msg)
+    send(msg, broadcast=True)
+
+@app.route('/')
+def chat():
+    flash('cau')
+    return redirect(url_for('example'))
+
+@app.route('/chat')
+def example():
+    return render_template('chat.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
